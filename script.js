876061217c6de356e5c9304bdea7def431824599
Aurélien VILMINOT <49270516+aurelien-vilminot@users.noreply.github.com>
2020-02-04 15:50:16 +0100

    Damier en classe

--- script.js
+++ script.js
@@ -14,19 +14,100 @@
 //     });
 // }) ();
 
-(function() {
-    $(document).ready(function () {
+// (function () {
+//     'use strict';
+//     $(document).ready(function () {
+//         let cssBlack =  {'width':'5vw', 'height':'5vw', 'background':'black', 'display':'inline-block'};
+//         let cssWhite = {'width':'5vw', 'height':'5vw', 'background':'white', 'display':'inline-block'};
+//
+//         let mon_click = function () {
+//             alert('test !')
+//         };
+//
+//         for (let y = 0 ; y < 8 ; ++y){
+//             let ligne = $('<div></div>');
+//             $('#damier').append(ligne);
+//             for (let x = 0 ; x < 8 ; ++x){
+//                 ligne.append(
+//                     $('<div></div>')
+//                         .css((x+y) % 2 ? cssBlack : cssWhite)
+//                         .append('&nbsp;')
+//                         .mouseenter(function () {
+//                             $(this).css({'background' : 'blue'});
+//                         })
+//                         .mouseleave(function () {
+//                             $(this).css((x+y) % 2 ? {'background' : 'black'} : {'background' : 'white'});
+//                         })
+//                 );
+//             }
+//         }
+//
+//     });
+// })();
 
-        for (let i = 0 ; i < 10 ; i++)
-        {
-            let maCaseColonne = $('<div>&nbsp;</div>').css({'background': 'blue', 'width' : '100%', 'height' : '4vw', 'border' : 'solid', 'display' : 'flex'});
-            $('#damier').append(maCaseColonne);
-            for (let j = 0 ; j < 10 ; j++)
-            {
-                let maCaseLigne = $('<div>&nbsp;</div>').css({'background': 'blue', 'width' : '4vw', 'height' : '4vw', 'border' : 'solid', 'display' : 'flex', 'flex-direction' : 'column'});
-                $(maCaseColonne).append(maCaseLigne);
+(function () {
+    'use strict';
+
+    let cssBlack =  {'width':'5vw', 'height':'5vw', 'background':'black', 'display':'inline-block'};
+    let cssWhite = {'width':'5vw', 'height':'5vw', 'background':'white', 'display':'inline-block'};
+
+    /*  Version Class moderne  */
+    class MonDamier {
+        constructor() {
+        }
+
+        mybuild (longueur, largeur, endroit) {
+            for (let y = 0 ; y < largeur ; ++y) {
+                let ligne = $('<div></div>');
+                $(endroit).append(ligne);
+                for (let x = 0; x < longueur; ++x) {
+                    ligne.append(
+                        $('<div></div>')
+                            .css((x + y) % 2 ? cssBlack : cssWhite)
+                            .append('&nbsp;')
+                            .mouseenter(function () {
+                                $(this).css({'background': 'blue'});
+                            })
+                            .mouseleave(function () {
+                                $(this).css((x + y) % 2 ? {'background': 'black'} : {'background': 'white'});
+                            })
+                    );
+                }
+            }
+        }
+    }
+
+    /*  Version Class ancienne  */
+
+    let Damier = function (longueur, largeur, endroit) {
+
+        this.build  = function () {
+            for (let y = 0 ; y < largeur ; ++y) {
+                let ligne = $('<div></div>');
+                $(endroit).append(ligne);
+                for (let x = 0; x < longueur; ++x) {
+                    ligne.append(
+                        $('<div></div>')
+                            .css((x + y) % 2 ? cssBlack : cssWhite)
+                            .append('&nbsp;')
+                            .mouseenter(function () {
+                                $(this).css({'background': 'blue'});
+                            })
+                            .mouseleave(function () {
+                                $(this).css((x + y) % 2 ? {'background': 'black'} : {'background': 'white'});
+                            })
+                    );
+                }
             }
         }
 
+    };
+
+    $(document).ready(function () {
+        // let monDamier = new Damier(8, 8,'#damier');
+        // monDamier.build();
+
+        let monDamier = new MonDamier();
+        monDamier.mybuild(8, 8,'#damier');
     });
-}) ();
+})();
